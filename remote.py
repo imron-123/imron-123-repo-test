bdfc3d7a6ec45da5d757e6a5d552695c1318dfa7
imron-123 <lapiimron@gmail.com>
2021-03-23 10:14:08 +0700

    Menambahkan kata pemuda itu pelopor

--- remote.py
+++ remote.py
@@ -1,3 +1,4 @@
 print("Hari ini saya belajar git dan github\n")
 print("Semoga hari sabtu belajar git dan github saya suadh selesai")
 print("Ayo semnagat")
+print("Pemuda itu pelobor bukan pengekor")
